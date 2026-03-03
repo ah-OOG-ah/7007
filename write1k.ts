5b277929071880c004359f9d9b9374b69c0f408e
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2026-03-03 00:09:00 -0500

    Update write1k.ts

--- write1k.ts
+++ write1k.ts
@@ -11,8 +11,8 @@ const romHex = `
 30 31 32 33 34 35 36 37 38 39 3a 3b 3c 3d 3e 3f`.trim().replaceAll("\n", " ").split(" ");
 
 // The ROM prints out one byte per two hex digits
-console.log(romHex);
-console.log("ROM size: " + (romHex.length));
+console.error(romHex);
+console.error("ROM size: " + (romHex.length));
 
 const maxSize = 1024;
 if (romHex.length > maxSize) {
@@ -22,13 +22,38 @@ if (romHex.length > maxSize) {
 
 // Load the object
 import romJson from "./dumps/1krom.json";
-romJson.ConstantVal;
 
-let id = 0;
-for (let byte of romHex) {
-    output.allNodes[id] = templateNode;
-    output.Input[id] = makeInput(id, parseInt(byte, 16));
-    ++id;
+// Load constant vals into a new array and sort them
+const oldConstants = romJson.ConstantVal;
+let newConstants = [...oldConstants];
+newConstants.sort((a, b) => {
+    if (a.x < b.x) return -1;
+    else if (b.x < a.x) return 1;
+
+    if (a.y < b.y) return -1;
+    else if (b.y < a.y) return 1;
+
+    return 0;
+});
+
+// Max length is the number of digits in the bytes
+// If we have less than 0xff bytes, this is floor(log16(count)) + 1
+const maxLen = Math.floor(Math.log2(newConstants.length) / Math.log2(16)) + 1;
+
+
+for (let i = 0; i < newConstants.length; ++i) {
+    let conStr = "";
+
+    for (let ii = 0; ii < 4; ++ii) {
+        let byteIdx = i * 4 + ii;
+        let byte = byteIdx < romHex.length ? romHex[byteIdx] : "0";
+        let byteStr = parseInt(byte, 16).toString(2).padStart(8, "0");
+
+        conStr = byteStr + conStr;
+    }
+
+    newConstants[i].customData.constructorParamaters[2] = conStr;
+    newConstants[i].label = (i % 4 === 0) ? "0x" + (i*4).toString(16).padStart(maxLen, "0") : "";
 }
 
-console.log(JSON.stringify(output));
+console.log(JSON.stringify(romJson));
