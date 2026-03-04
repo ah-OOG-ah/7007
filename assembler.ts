41b320c00e3a518f3a5392d1bed696bfbd4a66df
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2026-03-03 23:19:18 -0500

    Implement HLT and MOV

--- assembler.ts
+++ assembler.ts
@@ -1,12 +1,75 @@
 // Given a hard-coded assembly program, converts it to bits.
 
+import {write} from "node:fs";
+import write1k from "./write1k";
+
+const REGISTERS = [ "A", "B", "C", "D", "E", "H", "L", "M" ];
+
 const asm = `
+MOV A, 1H
+MOV B, 1H
 HLT
 `.trim();
 
 console.log("Assembly:")
 console.log(asm);
 
-for (const line of asm.split("\n")) {
+const asmArr = asm.split("\n");
 
+// While the output can be and likely is longer than this, there are at least this many output bytes
+let output = new Array<number>(asmArr.length)
+
+let i = 1;
+let idx = 0;
+
+for (const line of asmArr) {
+    const params = line.split(" ")
+
+    switch (params[0]) {
+        case "HLT": idx = decodeHLT(output, idx); break
+        case "MOV": idx = decodeMOV(output, idx, params); break
+        default: {
+            console.log("Invalid opcode detected at line %i: %s", i, line)
+            process.exit(1)
+        }
+    }
+
+    ++i;
 }
+
+// HLT
+function decodeHLT(output: number[], idx: number): number {
+    output[idx] = 0
+    return idx + 1
+}
+
+// MOV R1, R2    --- ex: MOV A, B
+// MOV R, M      --- ex: MOV A, M
+// MOV R, number --- ex: MOV A, 42H
+// MOV M, R      --- ex: MOV M, A
+// MOV M, number --- ex: MOV M, 42H
+function decodeMOV(output: number[], idx: number, params: string[]) {
+    const r1 = REGISTERS.indexOf(params[1][0]);
+    if (r1 < 0) {
+        throw new Error("Invalid parameter for MOV! Opcode: " + params)
+    }
+
+    const r2 = REGISTERS.indexOf(params[2][0]);
+    if (r2 < 0) {
+        // Loading some int -> R
+        output[idx++] = 0b0000_0110 | (r1 << 3)
+        output[idx++] = parseInt(params[2].slice(0, -1), 16)
+        return idx
+    }
+
+    // Loading R2 -> R1, M is register 0x7
+    output[idx++] = 0b1100_0000 | (r1 << 3) | r2
+    return idx
+}
+
+let writer = write1k(output)
+console.log(output.map((v) => {
+    return "0x" + v.toString(16).padStart(2, "0")
+}))
+
+await writer
--- write1k.ts
+++ write1k.ts
@@ -3,19 +3,6 @@
  */
 import * as fs from "node:fs";
 
-const romHex = `
-01`.trim().replaceAll("\n", " ").split(" ");
-
-// The ROM prints out one byte per two hex digits
-console.log(romHex);
-console.log("ROM size: " + (romHex.length));
-
-const maxSize = 1024;
-if (romHex.length > maxSize) {
-    console.log("ROM too large for 1k chip! Please shrink it.");
-    process.exit(1);
-}
-
 // Load the object
 import romJson from "./dumps/1krom.json";
 
@@ -34,22 +21,29 @@ newConstants.sort((a, b) => {
 
 // Max length is the number of digits in the bytes
 // If we have less than 0xff bytes, this is floor(log16(count)) + 1
-const maxLen = Math.floor(Math.log2(newConstants.length) / Math.log2(16)) + 1;
+const MAX_ADDR_LEN = Math.floor(Math.log2(newConstants.length) / Math.log2(16)) + 1;
+const MAX_ROM_SIZE = 1024;
 
-
-for (let i = 0; i < newConstants.length; ++i) {
-    let conStr = "";
-
-    for (let ii = 0; ii < 4; ++ii) {
-        let byteIdx = i * 4 + ii;
-        let byte = byteIdx < romHex.length ? romHex[byteIdx] : "0";
-        let byteStr = parseInt(byte, 16).toString(2).padStart(8, "0");
-
-        conStr = byteStr + conStr;
+export default async function write1k(romHex: number[]) {
+    if (romHex.length > MAX_ROM_SIZE) {
+        console.log("ROM too large for 1k chip! Please shrink it.");
+        process.exit(1);
     }
 
-    newConstants[i].customData.constructorParamaters[2] = conStr;
-    newConstants[i].label = (i % 4 === 0) ? "0x" + (i*4).toString(16).padStart(maxLen, "0") : "";
-}
+    for (let i = 0; i < newConstants.length; ++i) {
+        let conStr = "";
 
-fs.writeFileSync("./build/rom.json", JSON.stringify(romJson));
+        for (let ii = 0; ii < 4; ++ii) {
+            let byteIdx = i * 4 + ii;
+            let byte = byteIdx < romHex.length ? romHex[byteIdx] : "0";
+            let byteStr = byte.toString(2).padStart(8, "0");
+
+            conStr = byteStr + conStr;
+        }
+
+        newConstants[i].customData.constructorParamaters[2] = conStr;
+        newConstants[i].label = (i % 4 === 0) ? "0x" + (i*4).toString(16).padStart(MAX_ADDR_LEN, "0") : "";
+    }
+
+    fs.writeFileSync("./build/rom.json", JSON.stringify(romJson));
+}
