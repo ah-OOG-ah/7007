4f06f8b13c39eaffaa60658a8d2069c0dace1e1d
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2026-03-03 20:21:21 -0500

    start work on assembler

---
+++ assembler.ts
@@ -0,0 +1,12 @@
+// Given a hard-coded assembly program, converts it to bits.
+
+const asm = `
+HLT
+`.trim();
+
+console.log("Assembly:")
+console.log(asm);
+
+for (const line of asm.split("\n")) {
+
+}
--- write1k.ts
+++ write1k.ts
@@ -1,18 +1,14 @@
 /**
  * Unlike the romGen script, this one modifies an existing ROM chip.
  */
-
-import { CVNode, InputNode } from "./CVC";
+import * as fs from "node:fs";
 
 const romHex = `
-00 01 02 03 04 05 06 07 08 09 0a 0b 0c 0d 0e 0f
-10 11 12 13 14 15 16 17 18 19 1a 1b 1c 1d 1e 1f
-20 21 22 23 24 25 26 27 28 29 2a 2b 2c 2d 2e 2f
-30 31 32 33 34 35 36 37 38 39 3a 3b 3c 3d 3e 3f`.trim().replaceAll("\n", " ").split(" ");
+01`.trim().replaceAll("\n", " ").split(" ");
 
 // The ROM prints out one byte per two hex digits
-console.error(romHex);
-console.error("ROM size: " + (romHex.length));
+console.log(romHex);
+console.log("ROM size: " + (romHex.length));
 
 const maxSize = 1024;
 if (romHex.length > maxSize) {
@@ -56,4 +52,4 @@ for (let i = 0; i < newConstants.length; ++i) {
     newConstants[i].label = (i % 4 === 0) ? "0x" + (i*4).toString(16).padStart(maxLen, "0") : "";
 }
 
-console.log(JSON.stringify(romJson));
+fs.writeFileSync("./build/rom.json", JSON.stringify(romJson));
